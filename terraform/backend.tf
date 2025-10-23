3b1b7667886281917cf05b1bcfc998d1a8cd9a21
Avinash Deogirkar <avinash_deogirkar@yahoo.com>
2025-10-23 11:27:02 +0530

    Modified scripts & bacnkenf.tf for environment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "s3" {
-    # These values will be set by deployment scripts
-    # For local development, they can be passed via -backend-config
-  }
+    bucket = "twin-terraform-state-544523573688" # Required: Name of the S3 bucket
+    key    = "dev/terraform.tfstate" # Required: Path to the state file within the bucket
+    region = "ap-south-1" # Required: AWS region where the bucket resides
+    encrypt = true # Optional: Enable server-side encryption
+    dynamodb_table = "dynamodb_table=twin-terraform-locks" 
+    }
 }
